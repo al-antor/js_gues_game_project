6272d09b58c535893ccbe43a7ee5b7da86128064
al-antor <alantor420@gmail.com>
2025-03-27 21:24:09 +0600

    change to chance 3 to 5

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function randomNumber() {
-  let num = Math.floor(Math.random() * 10) + 1;
-  return num;
+    let num = Math.floor(Math.random() * 10) + 1;
+    return num;
 }
 function closeAll() {
-  const button = document.querySelector(".input");
-  button.style.display = "none";
-  const reset = document.querySelector(".reset");
-  reset.innerText = "Play Again";
+    const button = document.querySelector(".input");
+    button.style.display = "none";
+    const reset = document.querySelector(".reset");
+    reset.innerText = "Play Again";
 }
 function showList(arr) {
-  let a = arr.join(" ");
-  document.querySelector(".user-guess").innerText = a;
+    let a = arr.join(" ");
+    document.querySelector(".user-guess").innerText = a;
 }
 const submit = document.querySelector(".input");
 const result = document.querySelector(".result");
@@ -18,48 +18,48 @@ const remain = document.querySelector(".choise-remain");
 const reset = document.querySelector(".reset");
 let userValue = document.querySelector(".user-input").value;
 let random = randomNumber();
-let choiseRemain = 3;
+let choiseRemain = 5;
 let inputList = [];
 submit.addEventListener("click", (e) => {
-  let userValue = document.querySelector(".user-input").value;
-  if (userValue == random) {
-    closeAll();
-    choiseRemain--;
-    remain.innerText = choiseRemain.toString();
-    result.style.color = "green";
-    inputList.push(userValue);
-    result.innerText = "You Chose Correct One. You Win!";
-    showList(inputList);
-    submit.disabled = true;
-    document.querySelector("input").value = "";
-  } else {
-    choiseRemain--;
-    result.style.color = "red";
-    inputList.push(userValue);
-    remain.innerText = choiseRemain.toString();
-    if (choiseRemain == 0) {
-      closeAll();
-      result.innerText = `Your choise is worng. You Lose\n computer choose: ${random}`;
-      submit.disabled = true;
-      document.querySelector("input").value = "";
-      showList(inputList);
+    let userValue = document.querySelector(".user-input").value;
+    if (userValue == random) {
+        closeAll();
+        choiseRemain--;
+        remain.innerText = choiseRemain.toString();
+        result.style.color = "green";
+        inputList.push(userValue);
+        result.innerText = "You Chose Correct One. You Win!";
+        showList(inputList);
+        submit.disabled = true;
+        document.querySelector("input").value = "";
     } else {
-      result.innerText = "Your choise is worng";
-      document.querySelector("input").value = "";
-      showList(inputList);
+        choiseRemain--;
+        result.style.color = "red";
+        inputList.push(userValue);
+        remain.innerText = choiseRemain.toString();
+        if (choiseRemain == 0) {
+            closeAll();
+            result.innerText = `Your choise is worng. You Lose\n computer choose: ${random}`;
+            submit.disabled = true;
+            document.querySelector("input").value = "";
+            showList(inputList);
+        } else {
+            result.innerText = "Your choise is worng";
+            document.querySelector("input").value = "";
+            showList(inputList);
+        }
     }
-  }
 });
 
 reset.addEventListener("click", (e) => {
-  document.querySelector(".user-guess").innerText = "";
-  result.innerText = "";
-  remain.innerText = "3";
-  choiseRemain = 3;
-  document.querySelector("input").value = "";
-  random = randomNumber();
-  inputList = [];
-  submit.disabled = false;
-  submit.style.display = "inline-block";
-  reset.innerText = "Reset";
+    document.querySelector(".user-guess").innerText = "";
+    result.innerText = "";
+    remain.innerText = "3";
+    choiseRemain = 5;
+    document.querySelector("input").value = "";
+    random = randomNumber();
+    inputList = [];
+    submit.disabled = false;
+    submit.style.display = "inline-block";
+    reset.innerText = "Reset";
 });
